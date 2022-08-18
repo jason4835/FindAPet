beb3f6241960b0f341a04032f65dbcb7c909bb7d
Jason Paz <roboticmasterjp@gmail.com>
2022-08-17 21:33:23 -0400

    Co-authored-by: Hurera Ranjha <HureraRanjha@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 const database = firebase.database().ref();
 
+//test
+let email = "test@gmail.com";
+let password = "1234";
+
 //Login Page
 
 const loginForm = document.getElementById("login-form");
 const loginEmail = document.getElementById("login-email");
-const loginPassword = document.getElementById("login-password")
-const loginBtn = document.getElementById("login-btn")
+const loginPassword = document.getElementById("login-password");
+const loginBtn = document.getElementById("login-btn");
+const feedContainer = document.getElementById("feed-container");
+
+const loginBtnClick = (event) => {
+    event.preventDefault();
+
+    if (email === loginEmail.value && password === loginPassword.value) {
+        loginForm.style.display = "none";
+        feedContainer.style.display = "block";
+    } else {
+        alert("Password and/or Email are incorrect");
+    }
+};
+
+loginBtn.onclick = loginBtnClick;
 
 //Sign Up Page
 
@@ -15,4 +33,37 @@ const signUpUsername = document.getElementById("sign-up-username");
 const signUpPhoneNumber = document.getElementById("sign-up-phone-number");
 const signUpFirstName = document.getElementById("sign-up-first-name");
 const signUpLastName = document.getElementById("sign-up-last-name");
+const signUpPassword = document.getElementById("sign-up-password");
+const signUpPasswordConfirm = document.getElementById("sign-up-password-confirm");
 const signUpBtn = document.getElementById("signup-btn");
+
+const checkPassword = (event) => {
+    event.preventDefault();
+    if (signUpPassword === signUpPasswordConfirm) {
+        updateDB();
+        alert("Account Created");
+    } else {
+        alert("Password does not match up");
+    };
+};
+
+//updating DataBase
+const updateDB = (event) => {
+    event.preventDefault();
+
+    let data = {
+        EMAIL: signUpEmail.value,
+        PASSWORD: signUpPassword.value,
+        FIRSTNAME: signUpFirstName.value, 
+        LASTNAME: signUpLastName.value,
+        PHONENUMBER: signUpPhoneNumber.value
+    };
+
+    //TEMPORARY: check whats getting printed out
+    console.log(data);
+
+    //sends to database
+    database.push(data);
+};
+
+signUpBtn.onclick = checkPassword;
